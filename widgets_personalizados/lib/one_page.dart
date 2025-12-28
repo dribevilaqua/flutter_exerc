6b4a13013fb66adaa591936d65f506e8b631b821
Dri Bevilaqua <andrielebevilaqua@gmail.com>
2025-12-28 03:15:13 -0300

    alteracao

--- widgets_personalizados/lib/one_page.dart
+++ widgets_personalizados/lib/one_page.dart
@@ -1,20 +1,60 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:widgets_personalizados/widgets/custom_button_widget.dart';
 
-class OnePage extends StatelessWidget {
+class OnePage extends StatefulWidget {
   const OnePage({super.key});
+
+  @override
+  State<OnePage> createState() => _OnePageState();
+}
+//usando ValueNotifier 
+//class _OnePageState extends State<OnePage> {
+  //int valorAleatorio = 0;
+class _OnePageState extends State<OnePage> {
+  ValueNotifier<int> valorAleatorio = ValueNotifier<int>(0);
+
+random() async {
+  for (int i = 0; i < 100; i++) {
+    await Future.delayed(const Duration(seconds: 1));
+    //setState(() { não usa setState no ValueNotifier
+      //usando ValueNotifier
+      //valorAleatorio = Random().nextInt(100);
+      valorAleatorio.value = Random().nextInt(100);
+   // });
+  }
+}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    body: Center(
-      child: CustomButtonWidget(
-        disable: false,
-        onPressed: () {},
-        title:'Custom BTN',
-        titleSize: 20,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //usando ValueNotifier
+            //Text(
+              //'Valor é: $valorAleatorio',
+              //style: TextStyle(fontSize: 20),),
+
+            ValueListenableBuilder(
+              valueListenable: valorAleatorio, 
+              builder:(_, value, __) => Text(
+                'Valor é: $value',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+
+            SizedBox(height: 10),
+            CustomButtonWidget(
+              disable: false,
+              onPressed: () => random(),
+              title: 'Custom BTN',
+              titleSize: 18,
+            ),
+          ],
+        ),
       ),
-    ),
     );
   }
-} 
+}
